751ff18d90e3fd36563139c5dc8b204de7276e44
LautaroPedraza <106124570+LautaroPedraza@users.noreply.github.com>
2023-03-23 18:45:14 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import { ItemListContainer } from "./componentes/ItemListContainer/ItemListContainer";
+import NavBar from "./componentes/NavBar/NavBar";
+
+function App() {
+  const greeting = [
+    "Bienvenido a mi tienda",
+  ]
+  
+   
+}
+
+export default App;
